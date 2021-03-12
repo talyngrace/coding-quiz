6eab6b305111bba78c4ec093678c5077fe0189b2
Talyn Demi <talyndemi@Talyns-MacBook-Air.local>
2021-03-11 19:50:20 -0500

    added README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # coding-quiz
+
+## Description
+This site is designed to ask the user a series of questions in quiz form to test their coding knowledge. I had a lot of trouble working through this code and couldn't figure out the scoring. I'm planning on working on this some more.
+
+## Installation
+This page uses HTML, CSS, and JavaScript to implement the tasks.
+
+## Usage
+This page consists of a start button and when pressed reveals a question with four possible answer choices. The user is guided through the questions. 
+
+    
+[Screenshot 1](assets/screenshot1.png)
+    
+
+## Features
+This site features an HTML, CSS page, and a JavaScript page.
--- assets/script.js
+++ assets/script.js
@@ -4,58 +4,87 @@ const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 
-let shuffleQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex;
 
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question 
 // (not worried about timer yet)
 
-startButton.addEventListener("click", startGame)
 
+function countdown() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerCount.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // sendMessage();
+        }
+    }, 1000);
+}
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion();
+  countdown();
+})
+
+//
 
 function startGame() {
     console.log("You clicked");
-    startButton.classList.add('hide')
-    shuffleQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 };
+
+
 
 function setNextQuestion() {
     resetState()
-    showQuestions(shuffleQuestions[currentQuestionIndex])
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 };
 
-function showQuestions(question) {
-    questionElement.innerText = question.question
-    question.answer.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach((answer) => {
+      const button = document.createElement("button");
+      button.innerText = answer.text;
+      button.classList.add("btn");
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener("click", selectAnswer);
+      answerButtonsElement.appendChild(button);
     });
-};
+  };
 
 function resetState() {
+    clearStatusClass(document.body);
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 };
 
-function selectAnswer() {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })  //converting to an Array
-};
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach((button) => {
+      setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove("hide");
+    } else {
+      startButton.innerText = "Restart";
+      startButton.classList.remove("hide");
+    }
+  }
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
@@ -89,42 +118,42 @@ const questions = [
         ]
     },
     {
-        question: "Commonly used data types DO NOT include:",
+        question: "The condition in an if / else statement is enclosed within ____.",
         answers: [
-            { text: 'alerts', correct: true },
-            { text: 'string', correct: false },
-            { text: 'booleans', correct: false },
+            { text: 'quotes', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'parentheses', correct: true },
             { text: 'numbers', correct: false }
 
         ]
     },
     {
-        question: "Commonly used data types DO NOT include:",
+        question: "Arrays in JavaScript can be used to store ____.",
         answers: [
-            { text: 'alerts', correct: true },
-            { text: 'string', correct: false },
+            { text: 'numbers and strings', correct: false },
+            { text: 'other arrays', correct: false },
             { text: 'booleans', correct: false },
-            { text: 'numbers', correct: false }
+            { text: 'all of the above', correct: true }
 
         ]
     },
     {
-        question: "Commonly used data types DO NOT include:",
+        question: "String values must be enclosed within ____ when being assigned to variables",
         answers: [
-            { text: 'alerts', correct: true },
-            { text: 'string', correct: false },
-            { text: 'booleans', correct: false },
-            { text: 'numbers', correct: false }
+            { text: 'commas', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'quotes', correct: true },
+            { text: 'parentheses', correct: false }
 
         ]
     },
     {
-        question: "Commonly used data types DO NOT include:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: [
-            { text: 'alerts', correct: true },
-            { text: 'string', correct: false },
-            { text: 'booleans', correct: false },
-            { text: 'numbers', correct: false }
+            { text: 'JavaScript', correct: false },
+            { text: 'Terminal/bash', correct: false },
+            { text: 'for loops', correct: false },
+            { text: 'console.log', correct: true }
 
         ]
     }
